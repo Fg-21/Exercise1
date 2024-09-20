db0eff7503b1ff313383657db2787c7d9cadb967
Fran <Fran@DESKTOP-3JOP471>
2024-09-20 16:24:59 +0200

    Exercise1

--- src/exercise1/Exercise1.java
+++ src/exercise1/Exercise1.java
@@ -1,12 +1,10 @@
 package exercise1;
-
+//Modificación hecha en PC de Casa a 20/09/2024 a las 16:24 horas
 public class Exercise1 {
 
 	public static void main(String[] args) {
 		System.out.print("Hello, how are you? ");
 		System.out.println("Fine thanks.");
-		System.out.println("Fine thanks.");
-
 	}
 
 }
